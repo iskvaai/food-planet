1a0e1421545552e614a8a8079e2e51ce690a02e8
Meerim <meerione11@gmail.com>
2022-03-26 17:13:36 +0600

    feat: add Menu

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import React from "react";
 import FirstBlock from "./Components/FirstBlock/FirstBlock";
 import Header from "./Components/Header/Header";
+import Menu from "./Components/Menu/Menu";
 
 function App() {
   return (
-      <div className="food-planet" style={{marginLeft:"200px"}}>
+      <div className="food-planet" style={{marginLeft:"200px", marginRight:"200px", width:"1200px"}}>
         <Header/>
         <FirstBlock/>
+        <Menu/>
     </div>
   );
 }
---
+++ src/Components/Menu/Menu.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Menu = () =>{
+    return(
+        <div className="menu-container">
+            <nav>
+                <ul className="third-header" style={{display: "flex"}}>
+                    <li>Меню</li>
+                    <li>Пицца</li>
+                    <li>Бургер</li>
+                    <li>Суши</li>
+                    <li>Роллы</li>
+                    <li>Салаты</li>
+                    <li>Десерты</li>
+                    <li>Напитки</li>
+                </ul>
+            </nav>  
+            <div className="sort">
+                 <p>Сортировать  по:</p>
+                <select name="" id="">
+                    <option value="По умолчанию">По умолчанию</option>
+                </select>
+                
+            </div>  
+        </div>
+       
+    )
+}
+
+export default Menu;
