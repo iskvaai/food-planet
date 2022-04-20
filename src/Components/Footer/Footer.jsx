836760ba11c0a9bbcb0ab7c06820a5032f2838db
Meerim <meerione11@gmail.com>
2022-04-21 00:51:35 +0600

    fix

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,33 +1,5 @@
 import React from "react";
-<<<<<<< HEAD
-import whiteSpoon from "../../Media/white spoon.png";
-import styles from "./Footer.module.css";
-import whitePhone from "../../Media/white phone.png";
 
-const Footer = () => {
-    return(
-        <div className={styles.containerFooter} >
-            <img className={styles.whiteSpoon} src={whiteSpoon} />
-           <div className={styles.twoF}>
-                <p className={styles.food}>food planet</p>
-                <p className={styles.planet}>Планета вкусной еды</p>
-           </div>
-            <div className="lastMenu">
-                <nav>
-                    <ul className={styles.footerUl}>
-                        <li className={styles.firstLi}><a href="">Главная</a></li>
-                        <li> <a href="">Menu</a><select className={styles.footerSelect}></select></li>
-                        <li> <a href="">Доставка</a></li>
-                        <li> <a href="">Контакты</a></li>
-                        <img className={styles.whitePhone} src={whitePhone}/>
-                        <li className={styles.numberLi}><a href="">+996500405988</a></li>
-                    </ul>
-                </nav>
-            </div>
-        </div>
-    )
-}
-=======
 import styles from "./Footer.module.css"
 import phone from "../../Media/phone.svg";
 import korzina from "../../Media/korzina.svg";
@@ -59,5 +31,4 @@ const Footer = () => {
     );
 };
 
->>>>>>> origin/aidana
 export default Footer;
