0f1c68f84f1510471cb0cd6866ad09f6632ced1f
Meerim <meerione11@gmail.com>
2022-04-21 01:08:24 +0600

    fix

--- src/Components/Comments/Comments.jsx
+++ src/Components/Comments/Comments.jsx
@@ -6,21 +6,6 @@ import styles from "./Comments.module.css"
 const Comments = () => {
     return (
         <>
-<<<<<<< HEAD
-            <h1 className={styles.reviews}>Отзывы</h1>
-            {
-                commentary.map((item, index) => {
-                    return <CommentsItem
-                        key={index}
-                        img={item.img}
-                        name={item.name}
-                        titleDesc={item.titleDesc}
-                        data={item.data}
-
-                    />
-                })
-            }
-=======
            <div className={styles.mainDiv}>
                 <h1 className={styles.reviews}>Отзывы</h1>
                 {
@@ -36,7 +21,6 @@ const Comments = () => {
                     })
                 }
            </div>
->>>>>>> origin/aidana
         </>
     )
 }
--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,38 +1,4 @@
 import React from "react";
-<<<<<<< HEAD
-
-import styles from "./Footer.module.css"
-import phone from "../../Media/phone.svg";
-import korzina from "../../Media/korzina.svg";
-import spoon from "../../Media/spoon.png";
-
-const Footer = () => {
-    return (
-        <div style={{backgroundColor: "#254F4C"}}>
-            <footer>
-                <div >
-                    <img src={spoon} alt=""/>
-                    <p>food planet</p>
-                    <p>Планета вкусной еды</p>
-
-                <nav>
-                    <ul className={styles.firstMenu}>
-                        <li className={styles.firstLi}><a href="">Главная</a></li>
-                        <li><a href="">Menu</a></li>
-                        <li><a href="">Доставка</a></li>
-                        <li><a href="">Контакты</a></li>
-                        <img className={styles.phone} src={phone}/>
-                        <li className={styles.numberLi}><a href="">+996500405988</a></li>
-
-                    </ul>
-                </nav>
-                </div>
-            </footer>
-        </div>
-    );
-};
-
-=======
 import whiteSpoon from "../../Media/white spoon.png";
 import styles from "./Footer.module.css";
 import whitePhone from "../../Media/white phone.png";
@@ -60,5 +26,4 @@ const Footer = () => {
         </div>
     )
 }
->>>>>>> origin/aidana
 export default Footer;
